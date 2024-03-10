835434ef370dc9556928c6273e9a1af7da5af13c
Armond-Guze <Armond.Guze@yahoo.com>
2024-03-10 18:54:33 -0400

    started models

--- models/User.js
+++ models/User.js
@@ -1,1 +1,22 @@
-username
+const { Schema } = require("mongoose");
+
+const user = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // match: ,
+        },
+        thoughts: [
+            
+        ]
+
+    }
+)

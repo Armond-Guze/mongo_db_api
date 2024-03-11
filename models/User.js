adb2dd059412ff5d5e74d71f558bbf096f44a6d4
Armond-Guze <Armond.Guze@yahoo.com>
2024-03-10 23:35:06 -0400

    finished routes index, created User model

--- models/User.js
+++ models/User.js
@@ -1,22 +1,33 @@
+const { Module } = require("module");
 const { Schema } = require("mongoose");
 
-const user = new Schema(
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // match: ,
+  },
+  thoughts: [
     {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trimmed: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            // match: ,
-        },
-        thoughts: [
-            
-        ]
+      type: Schema.Types.ObjectId,
+      ref: "thoughts",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
-    }
-)
+const User = model("User", userSchema)
+
+Module.exports = User;

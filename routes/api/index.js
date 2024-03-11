b7feca7d4237349926d798ac7f9b42f6dbdaa99b
Armond-Guze <Armond.Guze@yahoo.com>
2024-03-11 00:30:41 -0400

    added readme slight changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router;
 const userRoute = require('./userRoute.js');
-const thoughtRoute = require('./thoughtRoute.js');
+// const thoughtRoute = require('./thoughtRoute.js');
 
 router.use('/user', userRoute);
-router.use('/thought', thoughtRoute);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const routes = require('./routes');
-const db = require('./config/connection.js');
+const express = require("express");
+const routes = require("./routes");
+const db = require("./config/connection.js");
 
 const PORT = 3001;
 const app = express();
 
+process.cwd()
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`\n running on port #${PORT}`)
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`\n running on port #${PORT}`);
+  });
 });
